e645897e0c1396649f83c82eb74e9634e66bcb72
Temsha H <temsha@anokiea.com>
2020-08-13 04:04:00 -0600

    save point

--- benfords_law.py
+++ benfords_law.py
@@ -1,4 +1,20 @@
 """Test provided CSV of values against Benford's Law"""
 
-import bendfordslaw
+import benfordslaw as bl
+
+# Load elections example
+df = bl.import_example()
+
+# Extract election information.
+X = df['votes'].loc[df['candidate']=='Donald Trump'].values
+
+# Print
+print(X)
+# array([ 5387, 23618,  1710, ...,    16,    21,     0], dtype=int64)
+
+# Make fit
+out = bl.fit(X)
+
+# Plot
+bl.plot(out, title='Donald Trump')
 
